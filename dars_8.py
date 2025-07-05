19f3bef35f982d1f21c362b4a1ff519ab652e31a
ZarifaHojimatova <zarifahojimatova907@gmail.com>
2025-07-05 22:16:45 +0500

    Create dars_8.py

---
+++ dars_8.py
@@ -0,0 +1,91 @@
+# List Methodlari,
+# mevalar =["olma,anor, behi"]
+# mevalar.append("shaftoli,anor,olcha")# ohiriga element qo'shadi.
+# print("mevalar")
+
+
+# mevalar =["olma,onor,nok"]
+# mevalar.insert(2,"nok")# listni istagan joyiga element
+# print(mevalar)       # qo'shadi,
+
+# mevalar = ["olma,anor,behi"]
+# mevalar_2=["shaftoli,uzum nok"]
+# # mevalar.extend(("shaftoli"))#list ohiriga bir nechta element qo'shadi
+# print(mevalar)
+#
+#
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+#
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist)) # nechta element borligini aniqlash uchun.len
+# ishlatiladi.
+#
+#String, int, boolenan malumot turlari.
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
+#
+# print(list1)
+# print(list2)
+# print(list3)
+#
+# list1 = ["abc", 34, True, 40, "male"]
+# print(list1)#list turli ma'lumotni o'z ichiga olishi mumkin.
+# #
+#
+# mevalar = ["olma," "anor", "nok"]
+# meva =  mevalar.pop(1)#  pop yulvoladi,3 ta funksiya bajaradi indeksini ol
+# print(meva)  # oladi va tengledi, va yulib olganini qaytaradi,
+# print(mevalar)
+#
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.remove("banana")# 3ta hususiysti bor[ 1- olma ichidegini qidirib
+# #o'chiradi, [2-ikkita kelib qolsa faqatgina birini o'chiradi,
+# #  [--3- topolmasa hato chikaradi,
+# print(fruits)
+#
+# fruits = ["apple", "banana", "cherry"]
+# fruits.clear()  bu ro'yhatni barcha elementini olib tashleydi,
+# print(fruits)
+# #
+# fruits = ["apple", "banana", "cherry"]
+# x = fruits.copy() #bu nusha olish..
+# x.append("0")
+# print(x)
+# print(fruits)
+
+
+
+
+# txt = "I love apples, apple are my favorite fruit"
+# x = txt.count("apple")        # sanash.ro'yhatta br hil elementni sonini
+# print(x)            # sanab beradi.
+
+
+# points =[1,2,5,6,8,9,8]
+# x = points.count(8) sanedi,
+# print(x)
+#
+# mevalar =("olma,anor,nok")
+# x =mevalar.index("nok")  #  ro'yhat sanab sonini chiqaradi'
+# print(x)
+
+#
+# fruits = [4, 55, 64, 32, 16, 32]
+# x = fruits.index(32)# birinchi indeks takrorlashini qaytaradi
+# print(x)
+#
+# cars = ['Ford', 'BMW', 'Volvo']
+# cars.sort(reverse=True)
+# print(cars)
+#
+
+# mashinalar =[ "Damas," "Tiko," "Nehiya"]
+# mashinalar.sort(reverse=True)
+# print(mashinalar)
+# #
+#
+# cars = ['Ford', 'BMW', 'Volvo','Hunda']
+# cars.sort(reverse=True)# RO'YHATNI KAMAYISHIGa QARAB TARTIBLENG,
+# print(cars)
