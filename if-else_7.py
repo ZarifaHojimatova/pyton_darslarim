e0fe0e7e440f5ec54d1c50d3f9e11509da44055c
ZarifaHojimatova <zarifahojimatova907@gmail.com>
2025-07-05 22:12:00 +0500

    Create if-else_7.py

---
+++ if-else_7.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  5 06:00:53 2025
+
+@author: user  Zarifa  Hojimatova 
+"""
+
+# avtolar = ['Damas','cobalt','lasetti','kia','tiko','treker']
+# for avto in avtolar:
+#     if avto =='cobalt':
+#         print(avto.upper())
+#     else:
+#         print(avto.title())
+
+
+# ism = input('ismingiz nima?')
+# if ism.lower() != 'ali':
+#     print(f" Uzr,{ism.title()} biz Alini kutyabmiz")
+# else:
+#     print("Salom ,Ali")
+
+# yosh =int(input('yoshingiz nechada? '))
+# if yosh>=18:    #  foydalanuvchini yoshini hisoblaymiz.
+#      print('hush kelibsiz.')
+# else:
+#     print('kirish mumkin emas')
+
+
+# a = 10
+# if a == 10:
+#     print('togri')
+# else:
+#     print('notogri')
+    
+
+
+# javob = float(input('5*5  nechaga teng' ))
+# if javob !=20:
+#     print('javob togri')
+# else:
+#     print('javob notogri')
+
+
+
+# login = input('Yangi login tanlang')
+# if len (login) <= 5:  login uzunligini tekshiramiz 
+#     print('login 5 harifdan iborat bolsin ')
+
+
+# yil = int(input('Tugilgan yilingizni kiriting: '))
+# if 2025-yil<18:
+#     print('yoshingiz{2025-yil} da ekan')
+#     print('kirish mumkin emas !')
+# else:
+#     print('xush kelibsiz ')
+
+
+# yosh = int(input('yoshingiz nechida? '))
+# if yosh > 65: print("siz COVID -19 guruhida ekansiz,")
+
+
+
+# x,y = 25,50
+# print("x>y") if x>y else print('x<y')
+
+
+
+
+# son =-32
+# if son <0:
+#     print('manfiy')
+# else:
+#     print('musbat')
+
+
+
+# yosh = int(input('yoshingiz nechida? '))
+# if yosh <=4:
+#     print('sizga kirish bepul')
+# elif yosh <= 12:
+#     print('sizga kirish 5000 som')
+# elif yosh <=18:
+#     print('sizga kirish 8000 som')
+# else:
+#     print('sizga kirish 1000 som')
+
+
+# yosh = int(input('yoshingiz nechida? '))
+# if yosh <=4:
+#     narh = 0
+# elif yosh <= 12:
+#     narh = 5000
+# elif yosh <=18:
+#     narh = 8000
+# else:
+#     narh = 10000
+# print(f'sizga kirish {narh} som')
+
+
+# kun = input('bugun nima kun?')
+# harorat = float(input('havo harorati qanday? '))
+# if kun.lower()=='yakshanba' and harorat >=30:
+#     print('chomilgani kettik')
+# elif kun.lower()== 'yakshanba 'and harorat<30:
+#     print('uyda dam olamiz')
+
+
+
+# Boolen True: False:
+
+# narh = 15000
+# choy = 2000
+# non = 3000
+# kampot = 5000
+# salat = 1000
+# kabob = 15000
+
+# if choy:
+#     print('mijoz choy oldi')
+#     narh = narh +200
+# if kabob:
+#     print('mijoz kabob oldi')
+#     narh = narh +15000
+# elif salat or kampot :
+#     narh = narh + 15000
+# print(f'jami  {narh} som')
+    
+
+
+
+# menu =['osh','manti','kabob','shorva','asarti']
+# ovqat = input('nima ovqat yiysiz? ')
+# if ovqat.lower() in menu:
+    
+#     print('buyurtma qabul qilindi ')
+# else:
+#     print('afsuski bizda bunday ovqat yoq ')
+
+
+# menu =['osh','manti','kabob','shorva','asarti']
+# buyurtma = ['osh','somsa','manti']
+# for taom in buyurtma:
+#     if taom in menu:
+#         print(f'menuda {taom} bor ')
+#     else:
+#         print(f'kechirasiz, menuda {taom} yoq')
+
+
+
+
+
+
+
+
+
+
+
