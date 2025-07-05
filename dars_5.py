96dacf2d388d850a532ac718375133584b6d547c
ZarifaHojimatova <zarifahojimatova907@gmail.com>
2025-07-05 22:03:57 +0500

    yangi darslar

---
+++ dars_5.py
@@ -0,0 +1,145 @@
+# a =5.2
+#
+# c = a ** 2
+# print(c)
+
+# son=24   varible  , var - 0'zgaruvchi
+# son = 30  int - integer  - butun son, -1, 0 , 123
+# print(son)
+# print(type(son))
+#
+
+# son2 = 3.2
+# print(type(son2)) kasir- float
+# #
+# matn = 'o\'zim'
+# # matn2 ="ozim"/   string - matnli malumot turi
+# print(matn)
+# print(type(matn2))
+#
+#
+# # ifoda = True      rost
+# ifoda = False       yolgon.
+# print(type(ifoda))  boolean -bool mantiqiy malumat turi.
+
+# data types malumot turlari,
+# int, float,str, bool, list,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ for_siqli_6.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  4 21:06:22 2025
+
+@author: user
+"""
+
+# mehmonlar = ['Ali','Vali','Hasan','Guli','Madina']
+# for mehmon in mehmonlar:
+#     print("Salom", mehmon)
+# print("Hayir",mehmon)
+
+
+
+# mehmonlar = ['Ali','Vali','Hasan','Guli','Madina']
+# for mehmon in mehmonlar:
+#     print(f"Hurmatli {mehmon},sizni 20 yanvar kuni nahorgi oshga taklif qilaman")
+#     print("Hurmat bilan Hojimatovlar oylasi\n")
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son ** 2} ga teng")
+
+
